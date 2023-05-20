4b23bdae80159390cd7f14b11f9cb19b7e0e02fc
mario-hess <office.hess@gmail.com>
2023-05-21 01:07:48 +0200

    :recycle: The little refactor of course

--- src/timer.rs
+++ src/timer.rs
@@ -1,22 +1,23 @@
-
 use std::time::Instant;
+
+const TICK_DURATION_MS: u128 = 16;
 
 pub struct Timer {
     delay_timer: u8,
-    elapsed_time: Instant,
+    last_timer_update: Instant,
 }
 
 impl Timer {
     pub fn new() -> Self {
         Self {
             delay_timer: 0,
-            elapsed_time: Instant::now(),
+            last_timer_update: Instant::now(),
         }
     }
 
     pub fn get_delay_timer(&mut self) -> u8 {
-        let ms_diff = (Instant::now() - self.elapsed_time).as_millis();
-        let ticks = ms_diff / 16;
+        let elapsed_time = Instant::now().duration_since(self.last_timer_update).as_millis();
+        let ticks = elapsed_time / TICK_DURATION_MS;
 
         if ticks >= self.delay_timer as u128 {
             0
@@ -27,6 +28,7 @@ impl Timer {
 
     pub fn set_delay_timer(&mut self, value: u8) {
         self.delay_timer = value;
-        self.elapsed_time = Instant::now();
+        self.last_timer_update = Instant::now();
     }
 }
+
