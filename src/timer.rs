ec9ca89d64d2955cc40b623308550e98737fa7f8
mario-hess <office.hess@gmail.com>
2023-05-21 01:29:40 +0200

    :sparkles: Timer refactor

--- src/timer.rs
+++ src/timer.rs
@@ -1,10 +1,11 @@
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
-const TICK_DURATION_MS: u128 = 16;
+const TICK_DURATION_MS: u64 = 16;
 
 pub struct Timer {
     delay_timer: u8,
     last_timer_update: Instant,
+    paused_duration: Duration,
 }
 
 impl Timer {
@@ -12,23 +13,42 @@ impl Timer {
         Self {
             delay_timer: 0,
             last_timer_update: Instant::now(),
+            paused_duration: Duration::default(),
         }
     }
 
     pub fn get_delay_timer(&mut self) -> u8 {
-        let elapsed_time = Instant::now().duration_since(self.last_timer_update).as_millis();
-        let ticks = elapsed_time / TICK_DURATION_MS;
+        let elapsed_time = Instant::now()
+            .duration_since(self.last_timer_update)
+            .saturating_sub(self.paused_duration);
 
-        if ticks >= self.delay_timer as u128 {
-            0
+        let elapsed_ticks = elapsed_time.as_millis() as u64 / TICK_DURATION_MS;
+
+        if elapsed_ticks >= self.delay_timer as u64 {
+            self.delay_timer = 0;
         } else {
-            self.delay_timer - ticks as u8
+            self.delay_timer -= elapsed_ticks as u8;
         }
+
+        self.last_timer_update += Duration::from_millis(elapsed_time.as_millis() as u64);
+        self.paused_duration = Duration::default();
+
+        self.delay_timer
     }
 
     pub fn set_delay_timer(&mut self, value: u8) {
         self.delay_timer = value;
         self.last_timer_update = Instant::now();
+        self.paused_duration = Duration::default();
+    }
+
+    pub fn pause(&mut self) {
+        self.paused_duration = Instant::now().duration_since(self.last_timer_update);
+    }
+
+    pub fn resume(&mut self) {
+        self.last_timer_update = Instant::now() - self.paused_duration;
+        self.paused_duration = Duration::default();
     }
 }
 
