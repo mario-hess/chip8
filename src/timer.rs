028f8975fed2934af34e9dbe2537f0b2e9b93910
mario-hess <office.hess@gmail.com>
2023-05-21 00:34:20 +0200

    :sparkles: Timer changed to fixed time-step approach

--- src/timer.rs
+++ src/timer.rs
@@ -1,31 +1,39 @@
-use std::time::Instant;
+use std::time::{Duration, Instant};
+
+const TICK_DURATION_MS: u64 = 16;
 
 pub struct Timer {
     delay_timer: u8,
-    elapsed_time: Instant,
+    last_timer_update: Instant,
 }
 
 impl Timer {
     pub fn new() -> Self {
         Self {
             delay_timer: 0,
-            elapsed_time: Instant::now(),
+            last_timer_update: Instant::now(),
         }
     }
 
     pub fn get_delay_timer(&mut self) -> u8 {
-        let ms_diff = (Instant::now() - self.elapsed_time).as_millis();
-        let ticks = ms_diff / 16;
+        let current_time = Instant::now();
+        let elapsed_time = current_time
+            .duration_since(self.last_timer_update)
+            .as_millis() as u64;
+        let elapsed_ticks = elapsed_time / TICK_DURATION_MS;
 
-        if ticks >= self.delay_timer as u128 {
+        if elapsed_ticks >= self.delay_timer as u64 {
+            self.last_timer_update = current_time;
             0
         } else {
-            self.delay_timer - ticks as u8
+            self.delay_timer -= elapsed_ticks as u8;
+            self.last_timer_update += Duration::from_millis(elapsed_ticks * TICK_DURATION_MS);
+            self.delay_timer
         }
     }
 
     pub fn set_delay_timer(&mut self, value: u8) {
         self.delay_timer = value;
-        self.elapsed_time = Instant::now();
+        self.last_timer_update = Instant::now();
     }
 }
